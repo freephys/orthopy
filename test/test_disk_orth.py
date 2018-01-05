7790b80787e16d4816af72212ee1828809de9099
Nico Schlömer <nico.schloemer@gmail.com>
2018-01-05 18:04:25 +0100

    more tests for disk

--- test/test_disk_orth.py
+++ test/test_disk_orth.py
@@ -3,18 +3,24 @@
 from __future__ import division, print_function
 
 import numpy
+import pytest
 import sympy
 
 import orthopy
 
 
-def test_integral0(n=4):
+@pytest.mark.parametrize(
+    'mu', [
+        sympy.Rational(1, 2),
+        sympy.Rational(3, 2),
+        ])
+def test_integral0(mu, n=4):
     '''Make sure that the polynomials are orthonormal
     '''
     x = sympy.Symbol('x')
     y = sympy.Symbol('y')
     vals = numpy.concatenate(
-        orthopy.disk.tree(n, numpy.array([x, y]), symbolic=True)
+        orthopy.disk.tree(n, numpy.array([x, y]), mu=mu, symbolic=True)
         )
 
     # Cartesian integration in sympy is bugged, cf.
@@ -22,43 +28,60 @@ def test_integral0(n=4):
     # Simply transform to polar coordinates for now.
     r = sympy.Symbol('r')
     phi = sympy.Symbol('phi')
+
+    one_half = sympy.Rational(1, 2)
+    W = (mu + one_half) * (1-r**2)**(mu-one_half)
+
     assert sympy.integrate(
-        r * vals[0].subs([(x, r*sympy.cos(phi)), (y, r*sympy.sin(phi))]),
+        W * r * vals[0].subs([(x, r*sympy.cos(phi)), (y, r*sympy.sin(phi))]),
         (r, 0, 1), (phi, 0, 2*sympy.pi)
         ) == sympy.sqrt(sympy.pi)
 
     for val in vals[1:]:
         assert sympy.integrate(
-            r * val.subs([(x, r*sympy.cos(phi)), (y, r*sympy.sin(phi))]),
+            W * r * val.subs([(x, r*sympy.cos(phi)), (y, r*sympy.sin(phi))]),
             (r, 0, 1), (phi, 0, 2*sympy.pi)
             ) == 0
 
     return
 
 
-def test_orthogonality(n=3):
+@pytest.mark.parametrize(
+    'mu', [
+        sympy.Rational(1, 2),
+        sympy.Rational(3, 2),
+        ])
+def test_orthogonality(mu, n=3):
     x = sympy.Symbol('x')
     y = sympy.Symbol('y')
     tree = numpy.concatenate(
-        orthopy.disk.tree(n, numpy.array([x, y]), symbolic=True)
+        orthopy.disk.tree(n, numpy.array([x, y]), mu=mu, symbolic=True)
         )
     vals = tree * numpy.roll(tree, 1, axis=0)
 
     r = sympy.Symbol('r')
     phi = sympy.Symbol('phi')
+
+    one_half = sympy.Rational(1, 2)
+    W = (mu + one_half) * (1-r**2)**(mu-one_half)
     for val in vals:
         assert sympy.integrate(
-            r * val.subs([(x, r*sympy.cos(phi)), (y, r*sympy.sin(phi))]),
+            W * r * val.subs([(x, r*sympy.cos(phi)), (y, r*sympy.sin(phi))]),
             (r, 0, 1), (phi, 0, 2*sympy.pi)
             ) == 0
     return
 
 
-def test_normality(n=4):
+@pytest.mark.parametrize(
+    'mu', [
+        sympy.Rational(1, 2),
+        sympy.Rational(3, 2),
+        ])
+def test_normality(mu, n=3):
     x = sympy.Symbol('x')
     y = sympy.Symbol('y')
     tree = numpy.concatenate(
-        orthopy.disk.tree(n, numpy.array([x, y]), symbolic=True)
+        orthopy.disk.tree(n, numpy.array([x, y]), mu=mu, symbolic=True)
         )
 
     # Cartesian integration in sympy is bugged, cf.
@@ -66,9 +89,13 @@ def test_normality(n=4):
     # Simply transform to polar coordinates for now.
     r = sympy.Symbol('r')
     phi = sympy.Symbol('phi')
+
+    one_half = sympy.Rational(1, 2)
+    W = (mu + one_half) * (1-r**2)**(mu-one_half)
     for val in tree:
+        val_r_phi = val.subs([(x, r*sympy.cos(phi)), (y, r*sympy.sin(phi))])
         assert sympy.integrate(
-            r * val.subs([(x, r*sympy.cos(phi)), (y, r*sympy.sin(phi))])**2,
+            W * r * val_r_phi**2,
             (r, 0, 1), (phi, 0, 2*sympy.pi)
             ) == 1
     return
